ec2a623aacd7514fd7e3d0c71c3e4e050c4fa0f0
RAlanWright <wright.alan88@gmail.com>
2021-09-12 15:46:17 -0500

    Added firebase to project and udpated editor to fix formatting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import ReactDOM from "react";
+import ReactDOM from 'react';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Student Education Achievement</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Student Education Achievement</h1>
+        </div>
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById('root'));
